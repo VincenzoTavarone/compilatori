359312910dc7b60396963f251c6e20dd5a4ca3ba
Vincenzo <enzotavarone@gmail.com>
2017-08-26 15:55:53 +0200

    parser completo (da testare)

--- src/esercizio3/Parser.java
+++ src/esercizio3/Parser.java
@@ -24,6 +24,10 @@ public class Parser {
 		this.tokens = tokens;
 	}
 	
+	private boolean isFinished(){
+		return tokens[pointer]==null;
+	}
+	
 	public boolean parse(){
 		/*
 		 * Servono almeno 3 token per formare 1 statement e almeno uno statement per formare un programma
@@ -31,10 +35,10 @@ public class Parser {
 		if(tokens[2]==null)
 			return false;
 		
-		while(tokens[pointer]!=null){
-			if(!program())
-				return false;
+		if(!program()){
+			return false;
 		}
+		
 		return true;
 	}
 	
@@ -56,26 +60,126 @@ public class Parser {
 	
 	public boolean program1(){
 		
-		if(!tokens[pointer].getName().equals("SEMI") && tokens[pointer+1]!=null)
+		if(!tokens[pointer].getName().equals("SEMI"))
+			return isFinished();
+		
+		pointer++;
+		if(!stat())
 			return false;
 		
 		pointer++;
+		if(!program1())
+			return false;
+		
 		return true;
 	}
 	
 	public boolean stat(){
+		
+		int backtrack = pointer;
+		
+		if(!matched_stat()){
+			pointer = backtrack;
+			return open_stat();
+		}
+		
 		return true;
 	}
 	
 	public boolean matched_stat(){
+		
+		if(tokens[pointer].getName().equals("IF")){
+			
+			pointer++;
+			if(!id_num())
+				return false;
+			
+			pointer++;
+			if(!relaz())
+				return false;
+			
+			pointer++;
+			if(!id_num())
+				return false;
+			
+			pointer++;
+			if(!tokens[pointer].getName().equals("THEN"))
+				return false;
+			
+			pointer++;
+			if(!matched_stat())
+				return false;
+			
+			pointer++;
+			if(!tokens[pointer].getName().equals("ELSE"))
+				return false;
+			
+			pointer++;
+			if(!matched_stat())
+				return false;
+				
+		}else{
+			if(!tokens[pointer].getName().equals("ID"))
+				return false;
+			
+			pointer++;
+			if(!tokens[pointer].getName().equals("ASSIGN"))
+				return false;
+			
+			pointer++;
+			if(!id_num())
+				return false;
+		}
+		
 		return true;
 	}
 	
 	public boolean open_stat(){
+		
+		if(!tokens[pointer].getName().equals("IF"))
+			return false;
+		
+		pointer++;
+		if(!id_num())
+			return false;
+		
+		pointer++;
+		if(!relaz())
+			return false;
+		
+		pointer++;
+		if(!id_num())
+			return false;
+		
+		pointer++;
+		if(!tokens[pointer].getName().equals("THEN"))
+			return false;
+		
+		pointer++;
+		if(!open_stat())
+			return false;
+		
 		return true;
 	}
 	
 	public boolean open_stat_1(){
+		
+		int backtrack = pointer;
+		
+		if(!stat()){
+			pointer = backtrack;
+			if(!matched_stat())
+				return false;
+			
+			pointer++;
+			if(!tokens[pointer].getName().equals("ELSE"))
+				return false;
+			
+			pointer++;
+			if(!open_stat())
+				return false;
+		}
+		
 		return true;
 	}
 	
