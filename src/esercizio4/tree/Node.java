89126ee99c4c41599792a0824e02e6e599e0a4be
Vincenzo <enzotavarone@gmail.com>
2017-09-06 14:52:05 +0200

    implementata struttura dati

---
+++ src/esercizio4/tree/Node.java
@@ -0,0 +1,59 @@
+package esercizio4.tree;
+
+import java.util.List;
+
+public class Node<T> {
+	
+	private Node<T> parent;
+	private T value;
+	private List<Node<T>> children;
+	
+	public Node(T value) {
+		this.setValue(value);
+	}
+
+	public T getValue() {
+		return value;
+	}
+
+	public void setValue(T value) {
+		this.value = value;
+	}
+
+	public List<Node<T>> getChildren() {
+		return children;
+	}
+	
+	public Node<T> getParent() {
+		return parent;
+	}
+
+	public void setParent(Node<T> parent) {
+		this.parent = parent;
+	}
+
+	public void setChildren(List<Node<T>> children) {
+		this.children = children;
+	}
+	
+	public void addChild(Node<T> node){
+		node.setParent(this);
+		children.add(node);
+	}
+	
+	public Node<T> removeChildAt(int index){
+		return children.remove(index);
+	}
+	
+	public boolean isRoor(){
+		return parent==null;
+	}
+	
+	public boolean isExternal(){
+		return children.isEmpty();
+	}
+	
+	public boolean isInternal(){
+		return !isExternal();
+	}
+}
---
+++ src/esercizio4/tree/Tree.java
@@ -0,0 +1,35 @@
+package esercizio4.tree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Tree<T> {
+	
+	private Node<T> root;
+	
+	public Tree(Node<T> root){
+		this.setRoot(root);
+	}
+
+	public Node<T> getRoot() {
+		return root;
+	}
+
+	public void setRoot(Node<T> root) {
+		this.root = root;
+	}
+	
+	public List<Node<T>> getListOfNodes(){
+		List<Node<T>> list = new ArrayList<Node<T>>();
+		preorder(root, list);
+		return list;
+	}
+	
+	private void preorder(Node<T> e, List<Node<T>> list){
+		list.add(e);
+		for (Node<T> node : e.getChildren()) {
+			preorder(node, list);
+		}
+	}
+
+}
