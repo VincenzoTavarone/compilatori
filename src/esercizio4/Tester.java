84514bff25caeea5e8e6ca708c3f8c7858516bec
Vincenzo <enzotavarone@gmail.com>
2017-10-02 16:14:58 +0200

    modifiche minori 4

--- src/esercizio4/Tester.java
+++ src/esercizio4/Tester.java
@@ -1,11 +1,11 @@
 package esercizio4;
 
 import java.io.File;
-import java.io.FileNotFoundException;
+//import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.IOException;
+//import java.io.IOException;
 
-import java_cup.runtime.Symbol;
+//import java_cup.runtime.Symbol;
 
 
 public class Tester {
@@ -15,28 +15,28 @@ public class Tester {
 		System.out.println("esercizio 4\n");
 		
 		File file = new File("src/esercizio4/input.txt");
-		FileReader input = null;
-		try {
-			input = new FileReader(file);
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
-		
-		Lexer lexer = new Lexer(input);
-		
-		Symbol symbol = null;
-		
-		try {
-			while((symbol = lexer.next_token()) != null){
-				System.out.println(Wrapper.getClassName(symbol.sym));
-				if(symbol.sym == sym.EOF)
-					break;
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		
-		System.out.println("\n");
+//		FileReader input = null;
+//		try {
+//			input = new FileReader(file);
+//		} catch (FileNotFoundException e) {
+//			e.printStackTrace();
+//		}
+//		
+//		Lexer lexer = new Lexer(input);
+//		
+//		Symbol symbol = null;
+//		
+//		try {
+//			while((symbol = lexer.next_token()) != null){
+//				System.out.println(Wrapper.getClassName(symbol.sym));
+//				if(symbol.sym == sym.EOF)
+//					break;
+//			}
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
+//		
+//		System.out.println("\n");
 		
 		GrammaticaCup parser = new GrammaticaCup(new Lexer(new FileReader(file)), "");
 		
